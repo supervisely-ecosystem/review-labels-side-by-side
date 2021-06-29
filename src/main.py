a1a11ec828aeb05c939f575a1abc19e0ea9ad671
DVMix <dvmixalkin@mail.ru>
2021-06-29 15:16:27 +0300

    show_gallery fn completed, add manual_selected_{image/figure}_changes

--- src/main.py
+++ src/main.py
@@ -1,45 +1,81 @@
 import supervisely_lib as sly
 import globals as g
+import ui
+
+ann_1 = None
 
 
 @g.my_app.callback("show_info")
 @sly.timeit
 def show_info(api: sly.Api, task_id, context, state, app_logger):
+    print('manual_selected_image_changed')
     print('context = ', context)
     print('state = ', state)
-    image = api.image.get_info_by_id(context['imageId'])
+    selected_lablers = state['userNames']
+    show_gallery(api, context, selected_lablers)
+    ui.objects.update_table(api, ann_1, state)
 
 
-def show_gallery(api, context):
+def show_gallery(api, context, selected_lablers):
+    global ann_1
+    print('Show_images: ', context)
+    if context['imageId'] is None:
+        pass
     image_id = context['imageId']
-    image = api.image.get_info_by_id(context['imageId'])
+    ann_1 = api.annotation.download(image_id)
+    if selected_lablers:
+        selected_labler_objs = []
+        for i in ann_1.annotation['objects']:
+            if i['labelerLogin'] in selected_lablers:
+                selected_labler_objs.append(i)
+        ann_1.annotation['objects'] = selected_labler_objs
+    content = {
+        "projectMeta": {
+            "classes": g.meta_['classes'],
+            "tags": []
+        },
+        "annotations": {
+            "ann_1": {
+                "url": api.image.get_info_by_id(image_id).full_storage_url,
+                "figures": ann_1.annotation['objects'],
+                "info": {"title": "original"}
+            }
+        },
+        "layout": [["ann_1"]]
+    }
+    # <pre>{{data.previewContent}}</pre> # to show code
+    options = {
+        "showOpacityInHeader": True,
+        "opacity": 0.8,
+        "fillRectangle": False,
+    }
     fields = [
-        {"field": "data.perClassExtendedTable", "payload": table_data},
-        # {"field": "data.perClassTable", "payload": {"columns": metrics.table_classes_columns,
-        #                                             "data": table_classes}},
-        {"field": "data.perClassLineChartOptions", "payload": line_chart_options},
-        {"field": "data.perClassLineChartSeries", "payload": line_chart_series}
+        {"field": "data.previewContent", "payload": content},
+        {"field": "data.previewOptions", "payload": options},
     ]
     api.app.set_fields(g.task_id, fields)
-    pass
+
+
+def manual_template(api, state, context):
+    print('context = ', context)
+    print('state = ', state)
+    selected_lablers = state['userNames']
+    show_gallery(api, context, selected_lablers)
+    ui.objects.update_table(api, ann_1, state)
 
 
 @g.my_app.callback("manual_selected_image_changed")
 @sly.timeit
 def manual_selected_image_changed(api: sly.Api, task_id, context, state, app_logger):
     print('manual_selected_image_changed')
-    print('context = ', context)
-    print('state = ', state)
-    show_gallery(api, context)
+    manual_template(api, state, context)
 
 
 @g.my_app.callback('manual_selected_figure_changed')
 @sly.timeit
 def manual_selected_figure_changed(api: sly.Api, task_id, context, state, app_logger):
     print('manual_selected_figure_changed')
-    print('context = ', context)
-    print('state = ', state)
-    show_gallery(api, context)
+    manual_template(api, state, context)
 
 
 def main():
@@ -50,13 +86,9 @@ def main():
 
     data = {}
     state = {}
+    ui.init(data, state)
 
-    name_to_id = dict()
-    for user_info in g.api.user.get_team_members(g.team_id):
-        name_to_id[user_info.login] = user_info.id
-    print(name_to_id)
-    data['userNames'] = []
-    data['options'] = [{"value": i, "label": i} for i in name_to_id]
+    g.my_app.compile_template(g.root_source_dir)
     g.my_app.run(data=data, state=state)
 
 
